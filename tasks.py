a7e563d6c4047fa02a44cf00e812993136b72ba8
Bharath1121 <bsenthil@calpoly.edu>
2022-03-04 16:32:09 -0800

    Finished task 2

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
 from Crypto.Hash import SHA256
 import random, string
 import datetime
-
-from numpy import binary_repr
+from bcrypt import *
+from nltk.corpus import words
 
 def hashArbitraryInput(input):
     bytesInput = str.encode(input) # converts string input into bytes
@@ -29,16 +29,48 @@ def findCollision(modVal, length):
     while(True):
         someInput = generateMessage()
         truncatedDigest = modifyDigest(hashArbitraryInput(someInput), modVal, length)
-        if truncatedDigest in myDict.keys():
+        if truncatedDigest in myDict:
             return myDict[truncatedDigest], truncatedDigest, someInput, len(myDict.keys())
         else:
             myDict[truncatedDigest] = someInput
             continue
 
+def readFile(fileName):
+    f = open(fileName, "r")
+    content = f.read()
+    listOfUsers = content.splitlines()
+    f.close()
+    return listOfUsers    
+
+def gatherInfo(userInfo):
+    info = userInfo.split(":")
+    username = info[0]
+    saltHash = info[1]
+    salt = ""
+    for i in range(len(saltHash)):
+        if i == 29:
+            break;
+        else:
+            salt += saltHash[i]
+    return username, salt, saltHash
+
+def findPassword(salt, saltHash, listOfWords):
+    bSalt = salt.encode()
+    for word in listOfWords:
+        bWord = word.encode()
+        myOutput = hashpw(bWord, bSalt)
+        testVal = myOutput.decode()
+        if testVal == saltHash:
+            return word
+        else:
+            continue
+    print("No password found!")
+
+
 
 def main():
-    # print(hashArbitraryInput("hello"))
-    # print(hashArbitraryInput("iello")) # iello has hamming distance of 1 bit with hello
+    print(hashArbitraryInput("hello"))
+    print(hashArbitraryInput("iello")) # iello has hamming distance of 1 bit with hello
 
     # print(hashArbitraryInput("a"))
     # print(hashArbitraryInput("b")) # a has hamming distance of 1 bit with b
@@ -46,19 +78,33 @@ def main():
     # print(hashArbitraryInput("BOB"))
     # print(hashArbitraryInput("BOA")) # BOB has hamming distance of 1 bit with BOA
 
-    hexString = 'ff'
-    addedVal = '3'
-    for i in range(8, 52, 2):
-        if i % 4 != 0:
-            hexString = addedVal + hexString
-        if i % 4 == 0 and i != 8:
-            hexString = hexString.replace("3", "f", 1)
-        print("Num of bits are: ", i)
-        start_time = datetime.datetime.now()
-        print(findCollision(hexString, i))
-        end_time = datetime.datetime.now()
-        print("Time elapsed: ", end_time - start_time)
-        print("----")
+    # hexString = 'ff'
+    # addedVal = '3'
+    # for i in range(8, 52, 2):
+    #     if i % 4 != 0:
+    #         hexString = addedVal + hexString
+    #     if i % 4 == 0 and i != 8:
+    #         hexString = hexString.replace("3", "f", 1)
+    #     print("Num of bits are: ", i)
+    #     start_time = datetime.datetime.now()
+    #     print(findCollision(hexString, i))
+    #     end_time = datetime.datetime.now()
+    #     print("Time elapsed: ", end_time - start_time)
+    #     print("----")
+
+    # listOfUsers = readFile("shadow.txt")
+    # listOfWords = words.words()
+    # newListOfWords = []
+    # for word in listOfWords:
+    #     if len(word) >= 6 and len(word) <= 10:
+    #         newListOfWords.append(word)
+    # for i in range(len(listOfUsers)):
+    #     username, salt, saltHash = gatherInfo(listOfUsers[i])
+    #     start_time = datetime.datetime.now()
+    #     password = findPassword(salt, saltHash, newListOfWords)
+    #     print(username, password, salt)
+    #     end_time = datetime.datetime.now()
+    #     print("Time elapsed: ", end_time - start_time)
 
 
 if __name__ == '__main__':
